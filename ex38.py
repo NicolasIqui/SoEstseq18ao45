7332833bb10fa598ed2031e8a975b5a48d70747e
NicolasIqui <142914085+NicolasIqui@users.noreply.github.com>
2026-03-25 03:10:05 -0300

    Update ex38.py
    eu estava com preguica na primeira vez que eu fiz ele entao fiz da forma errada agora esta certo

--- ex38.py
+++ ex38.py
@@ -1,20 +1,17 @@
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite um número: "))
-n3 = int(input("Digite um número: "))
-n4 = int(input("Digite um número: "))
-maior = n1
-menor = n1
-if n2 > maior:
-    maior = n2
-if n2 < menor:
-    menor = n2
-if n3 > maior:
-    maior = n3
-if n3 < menor:
-    menor = n3
-if n4 > maior:
-    maior = n4
-if n4 < menor:
-    menor = n4
-print("Maior:", maior)
-print("Menor:", menor)
+n1=0
+maior=0
+menor=0
+
+for i in range (100):
+    n1=int(input("digite um valor"))
+    if i == 0:
+        maior = n1
+        menor = n1
+    if (n1>=maior):
+        maior=n1
+    elif(n1<menor):
+        menor=n1
+    
+    print(maior,menor)
+
+print(maior,menor)
